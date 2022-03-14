2b0647bec251ea72824e5fb2106145f596105626
Sebastian LaVine <mail@smlavine.com>
2022-03-14 14:11:01 -0400

    Bind XK_Pause to "enter"
    This combined with commit 4d49484 will make it easier to quickly paste
    and enter info from the keyboard.

--- config.h
+++ config.h
@@ -381,6 +381,7 @@ static Key key[] = {
 	{ XK_ISO_Left_Tab,  ShiftMask,      "\033[Z",        0,    0},
 	{ XK_Return,        Mod1Mask,       "\033\r",        0,    0},
 	{ XK_Return,        XK_ANY_MOD,     "\r",            0,    0},
+	{ XK_Pause,         XK_ANY_MOD,     "\r",            0,    0},
 	{ XK_Insert,        ShiftMask,      "\033[4l",      -1,    0},
 	{ XK_Insert,        ShiftMask,      "\033[2;2~",    +1,    0},
 	{ XK_Insert,        ControlMask,    "\033[L",       -1,    0},
